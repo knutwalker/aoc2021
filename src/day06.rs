a0c064f3a201c8c58783d35127f2df74462a5427
Paul Horn <dev@knutwalker.engineer>
2021-12-06 06:19:54 +0100

    Day 6 solution

--- src/day06.rs
+++ src/day06.rs
@@ -1,16 +1,32 @@
 register!(
     "input/day6.txt";
-    run(input: parse u64) -> usize {
-        (part1(&input), part2(&input))
+    run(input: String) -> usize {
+        let numbers = input[0].split(',').flat_map(str::parse::<usize>).collect::<Vec<_>>();
+        (part1(&numbers), part2(&numbers))
     }
 );
 
-fn part1(items: &[u64]) -> usize {
-    0
+fn part1(items: &[usize]) -> usize {
+    model_fishes(items, 80)
 }
 
-fn part2(items: &[u64]) -> usize {
-    0
+fn part2(items: &[usize]) -> usize {
+    model_fishes(items, 256)
+}
+
+fn model_fishes(initial: &[usize], days: usize) -> usize {
+    let mut fishes = [0_usize; 9];
+
+    for &timer in initial {
+        fishes[timer] += 1;
+    }
+
+    for _ in 0..days {
+        fishes.rotate_left(1);
+        fishes[6] += fishes[8];
+    }
+
+    fishes.into_iter().sum()
 }
 
 #[cfg(test)]
@@ -20,16 +36,16 @@ mod tests {
 
     #[test]
     fn test_ex() {
-        let input = r#""#;
+        let input = r#"3,4,3,1,2"#;
         let (res1, res2) = Solver::run_on(input);
-        assert_eq!(res1, 0);
-        assert_eq!(res2, 0);
+        assert_eq!(res1, 5934);
+        assert_eq!(res2, 26984457539);
     }
 
     #[test]
     fn test() {
         let (res1, res2) = Solver::run_on_input();
-        assert_eq!(res1, 0);
-        assert_eq!(res2, 0);
+        assert_eq!(res1, 360610);
+        assert_eq!(res2, 1631629590423);
     }
 }
